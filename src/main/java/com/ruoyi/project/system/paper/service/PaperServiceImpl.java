7fd6c44342db6d3131fa9db55e199f416e811d50
chenshuichuan <chenshuichuan@users.noreply.github.com>
2019-05-19 12:08:35 +0800

    change paper delete

--- src/main/java/com/ruoyi/project/system/paper/service/PaperServiceImpl.java
+++ src/main/java/com/ruoyi/project/system/paper/service/PaperServiceImpl.java
@@ -24,117 +24,119 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 public class PaperServiceImpl implements IPaperService {
-    @Autowired
-    private PaperMapper paperMapper;
-    @Autowired
-    private PaperCommentMapper paperCommentMapper;
-    /**
-     * 查询论文信息
-     *
-     * @param id 论文ID
-     * @return 论文信息
-     */
-    @Override
-    public Paper selectPaperById(Integer id) {
-        return paperMapper.selectPaperById(id);
-    }
+  @Autowired
+  private PaperMapper paperMapper;
+  @Autowired
+  private PaperCommentMapper paperCommentMapper;
 
-    /**
-     * 查询论文列表
-     *
-     * @param paper 论文信息
-     * @return 论文集合
-     */
-    @Override
-    public List<Paper> selectPaperList(Paper paper) {
-        return paperMapper.selectPaperList(paper);
-    }
+  /**
+   * 查询论文信息
+   *
+   * @param id 论文ID
+   * @return 论文信息
+   */
+  @Override
+  public Paper selectPaperById(Integer id) {
+    return paperMapper.selectPaperById(id);
+  }
 
-    /**
-     * 新增论文
-     *
-     * @param paper 论文信息
-     * @return 结果
-     */
-    @Override
-    public int insertPaper(Paper paper) {
-        User user = ShiroUtils.getSysUser();
-        paper.setUser(user.getUserName());
-        paper.setIsPass(3);
-        paper.setIsReview(0);
-        paper.setDownloadTimes(0);
-        paper.setAppraisalTimes(0);
-        paper.setPreviewTimes(0);
-        paper.setCreateTime(new Date());
-        paper.setUpdateTime(new Date());
-        //paper.setStatus(3);
-        return paperMapper.insertPaper(paper);
-    }
+  /**
+   * 查询论文列表
+   *
+   * @param paper 论文信息
+   * @return 论文集合
+   */
+  @Override
+  public List<Paper> selectPaperList(Paper paper) {
+    return paperMapper.selectPaperList(paper);
+  }
 
-    /**
-     * 修改论文
-     *
-     * @param paper 论文信息
-     * @return 结果
-     */
-    @Override
-    public int updatePaper(Paper paper) {
-        paper.setUpdateTime(new Date());
-        return paperMapper.updatePaper(paper);
-    }
+  /**
+   * 新增论文
+   *
+   * @param paper 论文信息
+   * @return 结果
+   */
+  @Override
+  public int insertPaper(Paper paper) {
+    User user = ShiroUtils.getSysUser();
+    paper.setUser(user.getUserName());
+    paper.setIsPass(3);
+    paper.setIsReview(0);
+    paper.setDownloadTimes(0);
+    paper.setAppraisalTimes(0);
+    paper.setPreviewTimes(0);
+    paper.setCreateTime(new Date());
+    paper.setUpdateTime(new Date());
+    //paper.setStatus(3);
+    return paperMapper.insertPaper(paper);
+  }
 
-    /**
-     * 删除论文对象
-     *
-     * @param ids 需要删除的数据ID
-     * @return 结果
-     */
-    @Override
-    @Transactional
-    public int deletePaperByIds(String ids) {
-        paperMapper.deletePaperByIds(Convert.toStrArray(ids));
-        return paperCommentMapper.deletePaperCommentByPaperIds(Convert.toStrArray(ids));
-    }
+  /**
+   * 修改论文
+   *
+   * @param paper 论文信息
+   * @return 结果
+   */
+  @Override
+  public int updatePaper(Paper paper) {
+    paper.setUpdateTime(new Date());
+    return paperMapper.updatePaper(paper);
+  }
+
+  /**
+   * 删除论文对象
+   *
+   * @param ids 需要删除的数据ID
+   * @return 结果
+   */
+  @Override
+  @Transactional
+  public int deletePaperByIds(String ids) {
+    paperMapper.deletePaperByIds(Convert.toStrArray(ids));
+    paperCommentMapper.deletePaperCommentByPaperIds(Convert.toStrArray(ids));
+    return 1;
+  }
 
 
-    /**
-     * 修改文件上传
-     *
-     * @param files 文件上传信息
-     * @return 结果
-     */
-    @Override
-    @Transactional
-    public int updatePaper(Paper files, Boolean isFile) {
-        int rtn = 0;
-        //修改新数据
-        rtn = paperMapper.updatePaper(files);
-        if (rtn > 0) {
-            //先判断有没有文件
-            Paper oldFiles = paperMapper.selectPaperById(files.getId());
-            File file = new File(oldFiles.getFileUrl());
-            if (file.isFile()) {
-                if (files != null && isFile) {
-                    file.delete();
-                } else {
-                    file.renameTo(new File(files.getFileUrl()));
-                }
-            }
+  /**
+   * 修改文件上传
+   *
+   * @param files 文件上传信息
+   * @return 结果
+   */
+  @Override
+  @Transactional
+  public int updatePaper(Paper files, Boolean isFile) {
+    int rtn = 0;
+    //修改新数据
+    rtn = paperMapper.updatePaper(files);
+    if (rtn > 0) {
+      //先判断有没有文件
+      Paper oldFiles = paperMapper.selectPaperById(files.getId());
+      File file = new File(oldFiles.getFileUrl());
+      if (file.isFile()) {
+        if (files != null && isFile) {
+          file.delete();
+        } else {
+          file.renameTo(new File(files.getFileUrl()));
         }
-        return rtn;
+      }
     }
+    return rtn;
+  }
 
-    @Override
-    public Paper updatePaperInfoByPaper(Paper paper) {
+  @Override
+  public Paper updatePaperInfoByPaper(Paper paper) {
 
-        Paper paper1 = paperMapper.selectPaperById(paper.getId());
-        paper1.setFileUrl(paper.getFileUrl());
-        paper1.setPaperUrl(paper.getPaperUrl());
-        paper1.setTitle(paper.getTitle());
-        paper1.setAuthor(paper.getAuthor());
-        paper1.setPaperType(paper.getPaperType());
-        paper1.setIsPublic(paper.getIsPublic());
-        paper1.setStatus(paper.getStatus());
-        return paper1;
-    }
+    Paper paper1 = paperMapper.selectPaperById(paper.getId());
+    paper1.setFileUrl(paper.getFileUrl());
+    paper1.setPaperUrl(paper.getPaperUrl());
+    paper1.setTitle(paper.getTitle());
+    paper1.setAuthor(paper.getAuthor());
+    paper1.setPaperType(paper.getPaperType());
+    paper1.setIsPublic(paper.getIsPublic());
+    paper1.setStatus(paper.getStatus());
+    return paper1;
+  }
 }
